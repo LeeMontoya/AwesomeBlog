abc73c15559b49b2a471a89fba4cf78c430bcb8a
LeeMontoya <jleemontoya@gmail.com>
2014-05-13 15:25:05 -0600

    Work in progress
    My first blog

---
+++ AwesomeBlog/AwesomeBlog/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AwesomeBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //stetup 1. Set up database access
+        Models.myBlogEntities db = new Models.myBlogEntities();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var list = db.Posts.OrderByDescending(x => x.postDate);
+            return View(list);
+        }
+
+        public ActionResult Like(int id)
+        {
+            //get post from database
+            var post = db.Posts.Where(x => x.blogID == id).First();
+            //increment the likes
+            post.favorite += 1;
+            //save changes to the database
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        
+        public ActionResult AddComment (int id, FormCollection values)
+        {
+            //making a new comment object to add to our database
+            var comment = new Models.Comment();
+            //set the vaules on our object from the vaules passed in by the form
+            comment.postID = id;
+            comment.author = values["author"];
+            comment.body = values["body"];
+            comment.postedOn = DateTime.Now;
+            comment.streetCred = 0;
+
+            //add our comment to the database
+            db.Comments.Add(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AwesomeBlog/AwesomeBlog/Models/BlogValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace AwesomeBlog.Models
+{
+    [MetadataType(typeof(PostMetadata))]
+    public partial class Post
+    {
+
+    }
+
+    public class PostMetadata
+    {
+        [Required(ErrorMessage = "You need to enter a title."), Display(Name = "Title")]
+        public string title;
+
+        [Required(ErrorMessage = "Date required.") , Display(Name = "Dated Posted")]
+        public DateTime postDate;
+
+        [Required(ErrorMessage = "Author required.") , Display(Name = "Author")] 
+        public string author;
+
+        [Display(Name = "Post Image"), MaxLength(200)]
+        public string thumbnail;
+
+        [Required(ErrorMessage = "Body required."), Display(Name = "Content")]
+        public string body;
+
+        [Display(Name = "Likes")]
+        public string favorite;
+
+        [Display(Name = "Views")]
+        public int viewCount;
+
+        [Display(Name = "Tags")]
+        public string tags;
+    }
+}
